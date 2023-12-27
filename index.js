aafc4d98bc7a22f749ba1562eaeb2f659af35895
behradsiamaki <145423707+behradsiamaki@users.noreply.github.com>
2023-12-27 17:24:38 +0330

    add book part with localstorage & domcontentloaded

--- index.js
+++ index.js
@@ -1,40 +1,71 @@
-const inputText = document.querySelector('#add-book input');
+const inputText = document.querySelector("#add-book input");
 const link = document.querySelector("button");
-const checkBox = document.querySelector('#hide input');
-const heading = document.querySelectorAll('.title')[1];
-const ul = document.querySelector('ul');
-const spanDelete = `<span class="delete"> حذف</span>`
+const checkBox = document.querySelector("#hide input");
+const heading = document.querySelectorAll(".title")[1];
+const ul = document.querySelector("ul");
+const spanDelete = `<span class="delete"> حذف</span>`;
 
-inputText.value = '';
+inputText.value = "";
 
-link.addEventListener('click', function(e){
-    const spanName = document.createElement('span');
-    spanName.classname = 'name';
-    spanName.textContent = inputText.value;
+link.addEventListener("click", function (e) {
+  const spanName = document.createElement("span");
+  spanName.className = "name";
+  spanName.textContent = inputText.value;
 
-    const li = document.createElement('li');
+  const li = document.createElement("li");
+
+  li.appendChild(spanName);
+  li.innerHTML += spanDelete;
+
+  ul.appendChild(li);
+
+  storeToLocalStorege(inputText.value);
+
+  inputText.value = "";
+  e.preventDefault();
+});
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = localStorage.getItem("tasks").split(",");
+  }
+
+  for (let item of tasks) {
+    const spanName = document.createElement("span");
+    spanName.className = "name";
+    spanName.textContent = item;
+
+    const li = document.createElement("li");
+
     li.appendChild(spanName);
     li.innerHTML += spanDelete;
 
     ul.appendChild(li);
+  }
+});
 
-    inputText.value = '';
+function storeToLocalStorege(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = localStorage.getItem("tasks").split(",");
+  }
 
-    e.preventDefault();
-})
+  tasks.push(task);
 
-checkBox.addEventListener('click', function(e){
-    if(checkBox.checked){
-        heading.style.display = 'none';
-        ul.style.display = 'none';
-    }else{
-        heading.style.display = 'initial ';
-        ul.style.display = 'initial';
-    }
-})
+  localStorage.setItem("tasks", tasks);
+}
 
-
-
-
-
-
+checkBox.addEventListener("click", function (e) {
+  if (checkBox.checked) {
+    heading.style.display = "none";
+    ul.style.display = "none";
+  } else {
+    heading.style.display = "initial ";
+    ul.style.display = "initial";
+  }
+});
